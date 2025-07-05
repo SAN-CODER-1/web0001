d3e112cb7d871ae5fb7acfa7208c4424efb5d539
sanjay <san638233@gmail.com>
2025-07-05 07:14:23 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-
 // https://vitejs.dev/config/
 
-export default {
-  build: {
-    sourcemap: false, // default is false; just make sure
-  },
-};
+export default defineConfig({
+
+plugins: [react()],
+
+}) 
+
